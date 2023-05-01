eda2c2f3fc6bfb74e6ecd940ee0bd069bae4c891
Sam <sam.camhi@gmail.com>
2023-04-30 20:07:49 -0400

    Context Lab

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-/* Your Code Here */
+const createEmployeeRecord = (array) => {
+    let employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],    
+        };
+    return employeeRecord;
+}
 
+          
+const createEmployeeRecords = (array) => array.map(employee => createEmployeeRecord(employee))
+          
+function createTimeInEvent (date) {
+    let newArray = this.timeInEvents;
+    newArray.push({
+        type: "TimeIn",
+        hour: parseInt(date.slice(11, date.length)),
+        date: date.slice(0,10),
+        })
+    return this;
+}
+          
+function createTimeOutEvent (date) {
+    let newArray = this["timeOutEvents"];
+    newArray.push({
+        type: "TimeOut",
+        hour: parseInt(date.slice(11, date.length)),
+        date: date.slice(0,10),
+        })
+    return this;
+}
+          
+function hoursWorkedOnDate (workDate) {
+    let timeIn = this["timeInEvents"].find(date => date["date"] === workDate)["hour"];
+    let timeOut = this["timeOutEvents"].find(date => date["date"] === workDate)["hour"];
+    return (timeOut - timeIn)/100;
+}
+          
+function wagesEarnedOnDate (date) {
+    return hoursWorkedOnDate.call(this, date)*this["payPerHour"]
+}
+
+const findEmployeeByFirstName = (srcArray, firstName) => srcArray.find(employee => employee["firstName"] === firstName)
+          
+/* const allWagesFor = (obj) => {
+    let dateArray = obj["timeInEvents"].map(workDate => workDate["date"]);
+    let earnedArray = dateArray.map(date => wagesEarnedOnDate(obj, date));
+    return earnedArray.reduce((accum, currentValue) => accum + currentValue);
+} */
+          
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +72,7 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll (array) {
+    let payroll = array.map(employee => allWagesFor.call(employee));
+    return payroll.reduce((accum, currentValue) => accum + currentValue);
+}
